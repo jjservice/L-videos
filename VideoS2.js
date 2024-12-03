f0a030a34951020f3138381d7507c6408d74a26d
jjservice <jjfabian47@gmail.com>
2024-12-03 09:45:06 -0500

    Uid876

--- VideoS2.js
+++ VideoS2.js
@@ -1,10 +1,10 @@
 const videos = [
     {   
         id: 1, 
-        name: "TOUCHDOWN", 
-        artist: "Messiah x Myke Towers", 
-        img: "MessiahMikeTouchDownPic.jpg", 
-        video: "Messiah, Myke Towers - TOUCHDOWN (Video Oficial).mp4" 
+        name: "Hora de Salir", 
+        artist: "Chencho Corleone", 
+        img: "ChenchoHoraDeSalirPic.jpg", 
+        video: "Chencho Corleone - Hora de Salir (Official Video).mp4" 
     },
     {   
         id: 2, 
@@ -15,101 +15,101 @@ const videos = [
     },
     {   
         id: 3, 
-        name: "Triste Halloween", 
-        artist: "Pressure 9x19 x YOVNGCHIMI", 
-        img: "Pressure9YOVNGTristeHalloweenPic.jpg", 
-        video: "Pressure9x19YOVNGCHIMI - Triste Halloween (OfficialVideo).mp4" 
+        name: "TOUCHDOWN", 
+        artist: "Messiah x Myke Towers", 
+        img: "MessiahMikeTouchDownPic.jpg", 
+        video: "Messiah, Myke Towers - TOUCHDOWN (Video Oficial).mp4" 
     },
     {   
         id: 4, 
-        name: "Hora de Salir", 
-        artist: "Chencho Corleone", 
-        img: "ChenchoHoraDeSalirPic.jpg", 
-        video: "Chencho Corleone - Hora de Salir (Official Video).mp4" 
-    },
-    {   
-        id: 5, 
-        name: "Las Pistas Son De Yecko", 
-        artist: "Yecko ft Pressure 9x19", 
-        img: "LasPistasSonDeYeckoPic.jpg", 
-        video: "Yecko ft Pressure 9x19 - Las Pistas Son De Yecko (Official Video).mp4" 
-    },
-    {   
-        id: 6, 
-        name: "Pila De Cotorra", 
-        artist: "El Fother x Darlyn Nay", 
-        img: "FotherDarlinPilaDeCotorraPic.jpg", 
-        video: "El Fother x Darlyn Nay - Pila De Cotorra (Video Oficial).mp4" 
-    },
-    {   
-        id: 7, 
         name: "PRC", 
         artist: "Peso Pluma, Natanael Cano ", 
         img: "pesoplumapic.jpg", 
         video: "Peso Pluma, Natanael Cano - PRC (Video Oficial).mp4" 
     },
     {   
-        id: 8, 
+        id: 5, 
         name: "DORA", 
         artist: "FARIANA x El Alfa", 
         img: "elalfadorayouPic.jpg", 
         video: "FARIANA x El Alfa - DORA.mp4" 
     },
     {   
-        id: 9, 
+        id: 6, 
         name: "Por el Resto de Tu Vida", 
         artist: "Christian Nodal x TINI", 
         img: "ChrisNitiporelrestoSpotPic.jpg", 
         video: "Christian Nodal, TINI - Por el Resto de Tu Vida (Video Oficial).mp45" 
     },
     {   
-        id: 10, 
+        id: 7, 
         name: "Amantes & Amigos", 
         artist: "Arcángel x Sech", 
         img: "arcangelsechpicc.jpg", 
         video: "Arcángel, Sech - Amantes & Amigos (Video Oficial).mp4" 
     },
     {   
-        id: 11, 
+        id: 8, 
         name: "Shampoo de Coco", 
         artist: "Anuel AA", 
         img: "anuelshampooPic11.jpg", 
         video: "Anuel AA - Shampoo de Coco (Video Oficial).mp4" 
     },
     {   
-        id: 12, 
+        id: 9, 
         name: "La Mejor Versión De Mi", 
         artist: "Natti Natasha X Romeo Santos", 
         img: "NatiRomeoLamejorVersionPic.jpg", 
         video: "Natti Natasha X Romeo Santos - La Mejor Versión De Mi[Official Video].mp4" 
     },
     {   
-        id: 13, 
+        id: 10, 
         name: "Caramelo Remix ", 
         artist: "Ozuna x Karol G x Myke Towers", 
         img: "CarameloRemixPic.jpg", 
         video: "Ozuna x Karol G x Myke Towers - Caramelo Remix  (Video Oficial).mp4" 
     },
     {   
-        id: 14, 
+        id: 11, 
         name: "Corazòn de Acero", 
         artist: "Yiyo Sarante", 
         img: "YiyoCorazonDeAceroPic.jpg", 
         video: "Yiyo Sarante - Corazòn de Acero (Video Oficial).mp4" 
     },
     {   
-        id: 15, 
+        id: 12, 
         name: "ESTE", 
         artist: "El Alfa  El Jefe  x Nfasis", 
         img: "este_pic.jpg", 
         video: "El Alfa  El Jefe  x Nfasis - ESTE (Oficial Video).mp4" 
     },
     {   
-        id: 16, 
+        id: 13, 
         name: "Si Antes Te Hubiera Conocido", 
         artist: "Karol G", 
         img: "karolgsiantePIC.jpg", 
         video: "KAROL G - Si Antes Te Hubiera Conocido(video).mp4" 
+    },
+    {   
+        id: 14, 
+        name: "Triste Halloween", 
+        artist: "Pressure 9x19 x YOVNGCHIMI", 
+        img: "Pressure9YOVNGTristeHalloweenPic.jpg", 
+        video: "Pressure9x19YOVNGCHIMI - Triste Halloween (OfficialVideo).mp4" 
+    },
+    {   
+        id: 15, 
+        name: "Las Pistas Son De Yecko", 
+        artist: "Yecko ft Pressure 9x19", 
+        img: "LasPistasSonDeYeckoPic.jpg", 
+        video: "Yecko ft Pressure 9x19 - Las Pistas Son De Yecko (Official Video).mp4" 
+    },
+    {   
+        id: 16, 
+        name: "Pila De Cotorra", 
+        artist: "El Fother x Darlyn Nay", 
+        img: "FotherDarlinPilaDeCotorraPic.jpg", 
+        video: "El Fother x Darlyn Nay - Pila De Cotorra (Video Oficial).mp4" 
     },
     {   
         id: 17, 
